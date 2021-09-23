18142b1f1ccfe69ee954e1345db117355b3406ea
FatihHfz <fatih199832@hotmail.com>
2021-09-23 18:47:55 +0300

    Adding Music and Transferring Music to the Next Scene

---
+++ Assets/Scripts/muzikoynatma.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class muzikoynatma : MonoBehaviour
+{
+    static muzikoynatma TekMuzikOynatıcısı = null; //Geçici bellekte yer ayrılır ama olusmaz
+    void Awake() 
+    {
+        if(TekMuzikOynatıcısı != null) //Muzik oynatıcısı varsa
+        {
+            Destroy(gameObject); //görünmeyen müzik objesi yok edildi
+        }
+        else
+        {
+            TekMuzikOynatıcısı = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+        }    
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
