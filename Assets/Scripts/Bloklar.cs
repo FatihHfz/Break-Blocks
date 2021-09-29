cc73a2d1940bf8cd6a61add498efe59d52cd7cb4
FatihHfz <fatih199832@hotmail.com>
2021-09-29 21:23:18 +0300

    editing the scenes

--- Assets/Scripts/Bloklar.cs
+++ Assets/Scripts/Bloklar.cs
@@ -6,14 +6,22 @@ public class Bloklar : MonoBehaviour
 {
     public int can;
     private int VurulmaSayisi;
+    private SahneKontrolu sahneYoneticisi;
     
     void Start()
     {
         VurulmaSayisi = 0;
+        sahneYoneticisi = GameObject.FindObjectOfType<SahneKontrolu>();
     }
 
    private void OnCollisionEnter2D(Collision2D collision) 
    {
         VurulmaSayisi++;    
+        SonrakiSahne();
+   }
+
+   public void SonrakiSahne()
+   {
+       sahneYoneticisi.SonrakıSahne();
    }
 }
